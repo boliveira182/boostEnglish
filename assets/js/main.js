4a6a678d8a1116cb58b3dee1553801b22f5228c1
boliveira182 <128522764+boliveira182@users.noreply.github.com>
2023-05-05 11:56:24 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,92 +10,15 @@ const card = document.querySelector('.card');
 const card__traduzido = document.querySelector('.card__traduzido');
 const nivel__select = document.querySelector('.nivel');
 const deck = document.querySelectorAll('[deck]');
-
 const container = document.querySelector('.container');
 const menu__lista = document.querySelectorAll('.menu__lista');
 const nivel = document.querySelectorAll('[nivel]');
-
-
-deckSelecionado = 'geral';
-nivelSelecionado = 'iniciante';
-
-carregaPagina();
-
-deck.forEach(element => {
-    element.addEventListener('click', () => {
-        deckSelecionado = element.attributes.value.value;
-        carregaPagina();
-    })
-});
-
-nivel.forEach(element => {
-    element.addEventListener('click', () => {
-        nivelSelecionado = element.attributes.value.value;
-        carregaPagina();
-        mudaMenu();
-    })
-});
-
-
-function carregaPagina(){
-    if(deckSelecionado === 'geral'){
-        lista = JSON.parse(localStorage.getItem('dicionario')) || [];
-    }else{
-        lista = JSON.parse(localStorage.getItem('revisar')) || [];
-    }
-    
-    if(nivelSelecionado ==="iniciante"){
-        lista = lista.slice(0, 1000);
-    }
-    if(nivelSelecionado ==="intermediario"){
-        lista = lista.slice(0, 3000);
-    }
-
-    lista__conhecidas = JSON.parse(localStorage.getItem('conhecidas')) || [];
-    lista__revisar = JSON.parse(localStorage.getItem('revisar')) || [];
-    
-    if(lista.length === 0){
-        lista = preencheLista();
-        localStorage.setItem('dicionario', JSON.stringify(lista));
-    }
-
-
-
-    palavra = Math.floor(Math.random() * lista.length);
-    const mostraPalavra = document.querySelector('[palavra]');
-
-    if(lista[palavra][0].length > 11){
-        card.style.fontSize='0.6em';
-    }
-    mostraPalavra.innerHTML = lista[palavra][0];
-
-    const mostraPalavra_trad = document.querySelector('[palavra_trad]');
-    if(lista[palavra][1].length > 11){
-        card__traduzido.style.fontSize='0.6em';
-    }
-    mostraPalavra_trad.innerHTML = lista[palavra][1];
-
-    palavraGerada = lista[palavra];
-
-    const totalPalavras = document.querySelector('[total]');
-    totalPalavras.innerHTML = lista.length;
-
-    const totalPalavrasAprenidas = document.querySelector('[aprendidas]');
-    totalPalavrasAprenidas.innerHTML = lista__conhecidas.length;
-}
-    
 const deletar = document.querySelector('[deletar]');
 const criar = document.querySelector('[criar]');
 
-deletar.addEventListener('click', () => {
-    var palavras = JSON.parse(localStorage.getItem('dicionarioOriginal'));
-    localStorage.setItem('dicionario', JSON.stringify(palavras));
-    localStorage.setItem('conhecidas', JSON.stringify([]));
-    mudaMenu();
-    carregaPagina();
-})
+setaVariaveis();
 
-
+//Essa função permite a criação do banco de palavras que são mostradas aos usuários
 criar.addEventListener('click', () => {
     const lista_english = prompt("Digite a lista de palavras em inglês:");
     const textEng = lista_english.replace(/(\r\n|\n|\r)/gm, ".");
@@ -120,6 +43,101 @@ criar.addEventListener('click', () => {
 })
 
 
+//Essa função reseta os Decks e reinicia o programa
+deletar.addEventListener('click', () => {
+    lista = preencheLista();
+    localStorage.setItem('dicionario', JSON.stringify(lista));
+    localStorage.setItem('conhecidas', JSON.stringify([]));
+    localStorage.setItem('indices', JSON.stringify(['1000', '3000', '10000']));
+    window.location.reload();
+})
+
+
+//Essa função permite selecionar o Deck, se é o geral (contém todas as palavras) ou se é o revisar (contém as palavras marcadas para revisão)
+deck.forEach(element => {
+    element.addEventListener('click', () => {
+        deckSelecionado = element.attributes.value.value;
+        carregaPagina();
+    })
+});
+
+//Função que permite selecionar o nível de difículdade do programa
+nivel.forEach(element => {
+    element.addEventListener('click', () => {
+        nivelSelecionado = element.attributes.value.value;
+        carregaPagina();
+        mudaMenu();
+    })
+});
+
+
+
+carregaPagina();
+function carregaPagina(){
+    tamanho = JSON.parse(localStorage.getItem('indices')) || [];
+    revisao = 'nao';
+
+    if(deckSelecionado === 'geral'){
+        lista__completa = JSON.parse(localStorage.getItem('dicionario')) || [];
+        lista = lista__completa;
+        revisao = "nao"; 
+    }else{
+        lista = JSON.parse(localStorage.getItem('revisar')) || [];
+        lista__revisao = lista.length;
+        revisao = "sim";
+    }
+    
+    if(nivelSelecionado ==="iniciante"){  
+        itens = tamanho[0];
+        lista = lista.slice(0, itens);
+        indice = [tamanho[0]-1, tamanho[1]-1, tamanho[2]-1]
+    }
+    if(nivelSelecionado ==="intermediario"){
+        itens = tamanho[1];
+        lista = lista.slice(0, itens);
+        indice = [tamanho[0], tamanho[1]-1, tamanho[2]-1]
+    }
+    if(nivelSelecionado ==="avancado"){
+        itens = tamanho[2];
+        indice = [tamanho[0], tamanho[1], tamanho[2]-1]
+    }
+
+    lista__conhecidas = JSON.parse(localStorage.getItem('conhecidas')) || [];
+    lista__revisar = JSON.parse(localStorage.getItem('revisar')) || [];
+    
+    if(lista.length === 0){
+        lista = preencheLista();
+        localStorage.setItem('dicionario', JSON.stringify(lista));
+    }
+
+    palavra = Math.floor(Math.random() * lista.length);
+    const mostraPalavra = document.querySelector('[palavra]');
+
+    if(lista[palavra][0].length > 11){
+        card.style.fontSize='0.6em';
+    }
+    mostraPalavra.innerHTML = lista[palavra][0];
+
+    const mostraPalavra_trad = document.querySelector('[palavra_trad]');
+    if(lista[palavra][1].length > 11){
+        card__traduzido.style.fontSize='0.6em';
+    }
+    mostraPalavra_trad.innerHTML = lista[palavra][1];
+
+    palavraGerada = lista[palavra];
+
+    const totalPalavras = document.querySelector('[total]');
+
+    if(revisao ==='sim'){
+        totalPalavras.innerHTML = lista__revisao;
+    }else{
+        totalPalavras.innerHTML = lista.length;
+    }
+    
+    const totalPalavrasAprenidas = document.querySelector('[aprendidas]');
+    totalPalavrasAprenidas.innerHTML = lista__conhecidas.length;
+}
+    
 buttoms.forEach((elements) => {
     elements.addEventListener('click', (evento) => {
         evento.preventDefault();
@@ -133,10 +151,18 @@ buttoms.forEach((elements) => {
        
             localStorage.setItem('conhecidas', JSON.stringify(lista__conhecidas));
 
-            index = lista.indexOf(palavraGerada);
-            lista.splice(index, 1);
+            index = lista__completa.indexOf(palavraGerada);
+            lista__completa.splice(index, 1);
             
-            localStorage.setItem('dicionario', JSON.stringify(lista));
+            localStorage.setItem('dicionario', JSON.stringify(lista__completa));
+
+            localStorage.setItem('indices', JSON.stringify(indice));
+
+            if(revisao ==="sim"){
+                index = lista__revisar.indexOf(palavraGerada);
+                lista__revisar.splice(index, 1);
+                localStorage.setItem('revisar', JSON.stringify(lista__revisar));
+            }
 
             card.style.display = "flex";
             card__traduzido.style.display = "none";
@@ -200,6 +226,10 @@ function mudaFundo(){
     opcoes.classList.toggle("aparece__fundo");
 }
 
+function setaVariaveis(){
+    deckSelecionado = 'geral';
+    nivelSelecionado = 'iniciante';
+}
 
 
  
